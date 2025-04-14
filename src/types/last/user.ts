28b38b4f07beaf2f2c87421cfdf453aae77dcf68
AleksanderGgrishaev <ceo.head@gmail.com>
2025-04-14 18:41:15 +0300

    Store back to category

--- src/types/last/user.ts
+++
@@ -1,9 +0,0 @@
-import { type BaseEntity } from './base';
-
-export interface User extends BaseEntity {
-    name: string;
-    pin: string; // Зашифрованный PIN-код
-    role: string
-    avatar?: string;
-    isAdmin: boolean;
-  }
